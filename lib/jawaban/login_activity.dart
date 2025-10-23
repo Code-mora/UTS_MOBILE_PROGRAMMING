fe2e9efa4426f0a12360f077ebd1e00e768f39f5
Code-mora <hafizuddinahwaz790@gmail.com>
2025-10-23 21:02:07 +0700

    commit kelima

--- lib/jawaban/login_activity.dart
+++ lib/jawaban/login_activity.dart
@@ -7,29 +7,49 @@ class LoginActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 600,),
-                child: Image(
-                  height: 100,
-                  width: 100,
-                  image: AssetImage("gambar/Logo.png"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 600,),
+                  child: Image(
+                    height: 100,
+                    width: 100,
+                    image: AssetImage("gambar/Logo.png"),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Text("LKS\nMART", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.purple)),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text("LKS\nMART", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.purple)),
+                ),
+              ],
+            ),
+          
+          Padding(
+            padding: const EdgeInsets.only(left: 540),
+            child: Text("Sign In", style: TextStyle(color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 540),
+            child: Text("Enter your ID and password to sign in!", style: TextStyle(color: Colors.black45)),
           ),
         
-        Text("Sign In")
-        ],
+          Text("Email"),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Email",
+              border: BorderRadius.all(Radius.circular(15)),
+            ),
+          ),
+          ],
+        ),
       ),
+      
     );
   }
 }
