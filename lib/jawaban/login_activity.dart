7ac5816d520ff3b89668064582dcadcbd16d6f64
Code-mora <hafizuddinahwaz790@gmail.com>
2025-10-23 19:57:44 +0700

    commit kedua

---
+++ lib/jawaban/login_activity.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginActivity extends StatelessWidget {
+  const LoginActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        children: [
+            Container(
+            height: 250,
+            width: 150,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("gambar/Gudang.png"),
+                fit: BoxFit.cover
+              ),
+            ),    
+            ),
+            Text("LKS\nMART"),
+          ],
+      ),
+      );
+  }
+}
